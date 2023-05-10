f95fc45dcbaa5219d42ef991c0c3d4ec963c2bcb
Richard Lovell <richjavalabs@gmail.com>
2023-05-10 17:52:36 +1200

    Updated deps

--- src/components/templates/lists/list-4/list-4.tsx
+++ src/components/templates/lists/list-4/list-4.tsx
@@ -26,36 +26,42 @@ export default function List4({ content }: any) {
           <div className="grid grid-cols-1 gap-10 lg:grid-cols-5">
             <div className="h-full col-span-3 lg:order-last">
               <Link href={url}>
-                  <div className="relative transition-opacity rounded-lg h-96 lg:h-full hover:opacity-80">
-                    <Image
-                      className="bg-gray-100 rounded-lg"
-                      src={`${publicRuntimeConfig.BACKEND_URL || ""}${heroPost.attributes?.image?.data.attributes.url}`}
-                      layout="fill"
-                      objectFit="cover"
-                      alt=""
-                    />
-                  </div>
+                <div className="relative transition-opacity rounded-lg h-96 lg:h-full hover:opacity-80">
+                  <Image
+                    className="bg-gray-100 rounded-lg"
+                    src={`${publicRuntimeConfig.BACKEND_URL || ""}${
+                      heroPost.attributes?.image?.data.attributes.url
+                    }`}
+                    layout="fill"
+                    objectFit="cover"
+                    alt=""
+                  />
+                </div>
               </Link>
             </div>
             <div className="col-span-2 lg:py-20">
               <div className="flex items-center mb-4">
-                <p className="mb-0 text-sm capitalize preheading">
-                  {format(new Date(heroPost.attributes.date), "dd LLLL yyyy")}
-                </p>
+                {heroPost.attributes.date && (
+                  <p className="mb-0 text-sm capitalize preheading">
+                    {format(new Date(heroPost.attributes.date), "dd LLLL yyyy")}
+                  </p>
+                )}
                 <span className="mx-3 text-gray-400">|</span>
                 {heroPost.attributes.category && (
                   <Link className="no-underline hover:underline" href={`/`}>
-                      <p className="mb-0 text-sm capitalize">{heroPost.attributes.category}</p>
+                    <p className="mb-0 text-sm capitalize">
+                      {heroPost.attributes.category}
+                    </p>
                   </Link>
                 )}
               </div>
               <Link className="no-underline" href={url}>
-                  <h2 className="hover:text-gray-700 dark:hover:text-gray-200">{heroPost.attributes.title}</h2>
+                <h2 className="hover:text-gray-700 dark:hover:text-gray-200">
+                  {heroPost.attributes.title}
+                </h2>
               </Link>
               <p className="mb-10 text-lg">{heroPost.attributes.blurb}</p>
-              <Link href={url}>
-                Read Article
-              </Link>
+              <Link href={url}>Read Article</Link>
             </div>
           </div>
         )}

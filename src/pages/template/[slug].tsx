30211e33f29b368b1c39cd46099f51f8e5fc9c75
Richard Lovell <richardlovell@Richards-MacBook-Air.local>
2024-03-12 22:33:11 +1300

    Updated templates

--- src/pages/template/[slug].tsx
+++ src/pages/template/[slug].tsx
@@ -1,33 +1,30 @@
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { withRouter } from "next/router";
 import { getConfig, getData } from "@builtjs/theme";
-import Page from "@/lib/page";
-import { pageTypes } from "@/lib/constants";
-import fs from "fs";
-import path from "path";
+import Page from "../../lib/page";
+import { pageTypes } from "../../lib/constants";
 
 export default withRouter(Page);
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const pageType = "template";
-  const filePath = path.join(process.cwd(), "public", "data", "pages.json");
-  const jsonData = fs.readFileSync(filePath, "utf-8");
-  const pageData = JSON.parse(jsonData);
+  const pageType = 'template'
+  let pageData = await getData("/data/pages.json");
   let pages = pageData.pages.reduce(
-    (acc: any, page: any) =>
-      page.type === pageType ? [...acc, `/${pageType}/${page.slug}`] : acc,
+    (acc:any, page:any) =>
+      page.type === pageType ? [...acc, `/${pageType}/${page.name}`] : acc,
     []
   );
   return {
     paths: pages,
     fallback: true,
   };
-};
+}
 
-export const getStaticProps: GetStaticProps = async (context: any) => {
+export const getStaticProps: GetStaticProps = async (context:any) => {
   const { slug } = context.params;
   const config = await getConfig(slug, pageTypes.TEMPLATE);
   return {
-    props: { config },
+    props: { config }
   };
 };
+
